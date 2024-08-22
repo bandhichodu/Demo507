1ae62eca7fe617fafef2707c54be4a4cf56db962
pardhu045 <133522410+pardhu045@users.noreply.github.com>
2024-08-22 10:42:51 +0530

    Update Sample1.java

--- DevOps-1_507/src/com/lbrce/Sample1.java
+++ DevOps-1_507/src/com/lbrce/Sample1.java
@@ -4,6 +4,16 @@ public class Sample1 {
 	public static void main(String args[])
 	{
 		System.out.println("Hello sample1");
+		System.out.println("Hello Scrum master!");
 	}
+	
 
 }
+public class Model
+	{
+		public static void main(String [] args)
+		{
+                      System.out.println("Hi bro");
+		}
+	}
+
